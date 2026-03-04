9159564f5b6909b2b50137840180c98930690ba3
Kavindu0118 <kavinduavishka0118@gmail.com>
2026-03-03 16:24:02 -0800

    update all the testimotnials file names

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 import './About.css';
 import customerImg1 from '../assets/testimonials/Ines.jpeg'
 import customerImg2 from '../assets/testimonials/valentin.jpeg'
-import customerImg3 from '../assets/testimonials/Talis.jpeg'
+import customerImg3 from '../assets/testimonials/talis.jpeg'
 import customerImg4 from '../assets/testimonials/customer4.png'
 
 const About = () => {
